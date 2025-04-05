721fb8e9ed9f9dd554abc2f01cd75330a279952a
amitjoshi24 <amit.joshiusa@gmail.com>
2025-04-05 12:41:10 -0400

    Initial Commit

---
+++ NoUltraWideCamera/NoUltraWideCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoUltraWideCameraApp.swift
+//  NoUltraWideCamera
+//
+//  Created by Amit Joshi on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct NoUltraWideCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoUltraWideCameraTests/NoUltraWideCameraTests.swift
@@ -0,0 +1,16 @@
+//
+//  NoUltraWideCameraTests.swift
+//  NoUltraWideCameraTests
+//
+//  Created by Amit Joshi on 4/5/25.
+//
+
+import Testing
+
+struct NoUltraWideCameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
